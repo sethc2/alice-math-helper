f28edc572993e0460b51fd12a7820c5ca030b472
Seth Copeland <sethc2@gmail.com>
2021-01-05 22:06:08 -0800

    First pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getNext() {
+  return getRandomInt(9) + 1;
+}
+
+function getSet() {
+  const numerator = getNext();
+  const denominator = getNext();
+
+  const set = new Set();
+  set.add(numerator * denominator);
+  while (set.size < 4) {
+    set.add(getNext() * denominator);
+  }
+
+  const answers = Array.from(set);
+  answers.sort((a, b) => a - b);
+  return { numerator, denominator, answers };
+}
 
 function App() {
+  const [
+    { numerator, denominator, answers },
+    setCurrentProblem,
+  ] = useState(() => getSet());
+  const [numCorrect, setNumCorrect] = useState(0);
+  const [numWrong, setNumWrong] = useState(60);
+
+  const [timeRemaining, setTimeRemaining] = useState(0);
+
+  useEffect(() => {
+    if (timeRemaining <= 0) {
+    } else {
+      setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
+  const onStart = () => {
+    setNumWrong(0);
+    setNumWrong(0);
+    setTimeRemaining(60);
+  };
+
+  const onSelectAnswer = (answer) => {
+    if (timeRemaining) {
+      const actualAnswer = numerator * denominator;
+      if (answer === actualAnswer) {
+        setNumCorrect(numCorrect + 1);
+      } else {
+        setNumWrong(numWrong + 1);
+      }
+      setCurrentProblem(getSet());
+    }
+  };
+  useEffect(() => {}, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          flex: "1",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <button onClick={onStart}>Start</button>
+          <span>Time left: {timeRemaining}s</span>
+        </div>
+        <div
+          style={{
+            fontSize: "60px",
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {numerator} X {denominator}
+        </div>
+      </div>
+      <div className="Answer">
+        {answers.map((answer) => (
+          <div className="AnswerDiv">
+            <button onClick={() => onSelectAnswer(answer)}>{answer}</button>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          flex: "1",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          fontSize: "60px",
+        }}
+      >
+        <div>Correct: {numCorrect}</div>
+        <div>Wrong: {numWrong}</div>
+      </div>
     </div>
   );
 }
